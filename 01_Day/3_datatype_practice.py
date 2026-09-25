81959a5390e893a87b8efe14024c06625460be91
Tapaswini005 <shawtapaswini2005@gmail.com>
2026-09-25 22:48:10 +0530

    Update file

--- 01_Day/3_datatype_practice.py
+++ 01_Day/3_datatype_practice.py
@@ -2,6 +2,8 @@ age = 20           # int
 height = 5.4       # float
 name = "Tapaswini" # string
 student = True     # boolean
+gender = "F"       # character
 
 print(type(age))
 print(type(name))
+print(type(gender))
